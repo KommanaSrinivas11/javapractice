70569daf961a35a38611f7d6908a135a64271551
srinivas <srinivas.kommana@zemosolabs.com>
2024-05-18 12:34:40 -0500

    18 may 2024

--- src/main/java/com/java/multithreading/multiThreading.java
+++ src/main/java/com/java/multithreading/multiThreading.java
@@ -1,5 +1,16 @@
 package com.java.multithreading;
 
-public class multiThreading {
+public class multiThreading extends Thread {
 
+	public void run()
+	{
+		System.out.println("Thread is running");
+	}
+
+public static void main(String args[])
+{
+	multiThreading m= new multiThreading();
+	m.start();
+	
+}
 }
