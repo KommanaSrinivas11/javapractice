acb3eb3d4dd8e14653775fbb671300a01e5429e8
srinivas <srinivas.kommana@zemosolabs.com>
2024-05-18 13:37:22 -0500

    commit

--- src/main/java/com/java/multithreading/multiThreading.java
+++ src/main/java/com/java/multithreading/multiThreading.java
@@ -5,13 +5,41 @@ public class multiThreading extends Thread {
 	public void run()
 	
 	{
-		System.out.println("to check");
+		for (int i=0;i<5;i++)
+		{
+		System.out.println(i);
+		try {
+		
+		
+		Thread.sleep(3000);
+		
+		
+		
+		}
+		catch(InterruptedException e)
+		{
+		System.out.println("Exception occured");
+		
+		}
+		
+	}
 	}
 
 public static void main(String args[])
 {
 	multiThreading m= new multiThreading();
+	multiThreading n= new multiThreading();
 	m.start();
 	
+	try {
+	m.join();
+		
+	}
+	catch(InterruptedException e)
+	{
+		System.out.println(e);
+	}
+	
+	n.start();
 }
 }
