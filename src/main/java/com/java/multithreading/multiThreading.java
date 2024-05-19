1ee9d2f5fe301d1dcbd282471fc818c942adcc22
srinivas <srinivas.kommana@zemosolabs.com>
2024-05-19 13:28:45 -0500

    sych

--- src/main/java/com/java/multithreading/multiThreading.java
+++ src/main/java/com/java/multithreading/multiThreading.java
@@ -5,41 +5,18 @@ public class multiThreading extends Thread {
 	public void run()
 	
 	{
-		for (int i=0;i<5;i++)
-		{
-		System.out.println(i);
-		try {
-		
-		
-		Thread.sleep(3000);
-		
-		
-		
-		}
-		catch(InterruptedException e)
-		{
-		System.out.println("Exception occured");
-		
-		}
-		
+	System.out.println(Thread.currentThread().getName())	;
 	}
-	}
+		
 
 public static void main(String args[])
 {
 	multiThreading m= new multiThreading();
-	multiThreading n= new multiThreading();
+	
 	m.start();
-	
-	try {
-	m.join();
-		
-	}
-	catch(InterruptedException e)
-	{
-		System.out.println(e);
-	}
-	
-	n.start();
+	String n=m.getName();
+	System.out.println("Thread Name"+n);
+	m.setName("My Thread");
+	System.out.println("Thread priority"+m.getPriority());
 }
 }
---
+++ src/main/java/com/java/multithreading/sychronized.java
@@ -0,0 +1,43 @@
+package com.java.multithreading;
+
+
+public class sychronized extends Thread {
+	
+	public void run()
+	{
+		for(int i=0;i<5;i++)
+		{
+			System.out.println(Thread.currentThread().getName() +" "+i);
+		}
+		synchronized(this)
+		{
+			try {
+			for (int i=0;i<5;i++)
+			{
+				System.out.println(i*i);
+				Thread.sleep(100);
+				
+			}
+			}
+			catch(InterruptedException e)
+			{
+				System.out.println(e);
+			}
+		}
+	}
+	
+
+public static void main(String args[])
+{
+	sychronized s=new sychronized();
+	
+	sychronized m=new sychronized();
+	sychronized k=new sychronized();
+	s.start();
+	m.start();
+	k.start();
+	
+	
+}
+
+}
